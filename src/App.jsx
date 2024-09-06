7eebc47f52e6d7c8d8e30681a68bf17c3e9e247e
bearden86 <bearden86@gmail.com>
2024-09-05 21:01:14 -0400

    finished up working error-free

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { useState } from "react";
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
+import Red from "./components/Red.jsx";
+import Blue from "./components/Blue.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div id="container">
         <div id="navbar">
           <Link to="/blue">blue</Link>
+          <Link to="/">home</Link>
           <Link to="/red">red</Link>
         </div>
         <div id="main-section">
           <Routes>
-            <Route path="/blue" element={<h1>Blue</h1>} />
-            <Route path="/red" element={<h1>Red</h1>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
           </Routes>
         </div>
       </div>
